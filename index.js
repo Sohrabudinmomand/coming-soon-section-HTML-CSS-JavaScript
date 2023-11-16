7af091c1019fb6f3b133f544adbc7fc25e2c211f
Sohrab momand <sohrabmomand.work@gmail.com>
2023-11-16 18:02:13 +0430

    final version of the project

--- index.js
+++ index.js
@@ -1,44 +1,22 @@
-let targetDate = localStorage.getItem('tergetDate');
+
+// // // Update the countdown every second
+setInterval(startCountDown, 1000);
 
 
-// Set the target date how many day you want from now
-if (!targetDate) {
-    targetDate = new Date();
-    // targetDate.setDate(targetDate.getDate() + 2);
-    targetDate.setFullYear(targetDate.getFullYear() + 2);
-    localStorage.setItem('tergetDate', targetDate);
-} else {
-    targetDate = new Date(targetDate);
-}
+function startCountDown() {
 
-// Update the countdown every second
-const countdown = setInterval(updateCountdown, 1000);
+    var targetTime = new Date('Dec 17, 2023 00:00:00').getTime();
+    var currentTime = new Date().getTime();
 
-function updateCountdown() {
-    const currentDate = new Date();
-    const timeDifference = targetDate - currentDate;
+    var remainingTime = targetTime - currentTime;
 
-    const days = Math.floor(timeDifference / (10000 * 60 * 60 * 24));
-    const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+    const days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
 
     document.getElementById('days').textContent = days;
     document.getElementById('hours').textContent = hours;
     document.getElementById('minutes').textContent = minutes;
     document.getElementById('seconds').textContent = seconds;
-
-    // If the countdown is over, stop the timer
-    if (timeDifference <= 0) {
-        clearInterval(countdown);
-        document.getElementById('countdown').textContent = 'Countdown expired!';
-    }
 }
-
-window.onload = function () {
-    updateCountdown()
-}
-
-
-// Initial call to set up the countdown
-updateCountdown();
